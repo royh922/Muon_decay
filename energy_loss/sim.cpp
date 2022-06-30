3fc80e5c1f72256dcf08d827d8394568a1ebc2a3
jthsu2001 <roy.h9222001@gmail.com>
2022-06-30 11:51:04 -0400

    create energy_loss portion

---
+++ energy_loss/sim.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cmath>
+#include <fstream>
+
+#define C 299792.458 //unit: m/s
+#define tau 2.1969811e-6 //unit: s
+#define m_0 105.6583755 //unit: MeV/c^2
+#define pi (atan(1)*4)
+
+ofstream output("sim.txt");
+
+double 
+
+int main(){
+
+}
