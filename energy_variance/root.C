7c8655df5af352d3c569b428cf4ebf60fd2bab10
royh922 <hjuiteng@gmail.com>
2022-07-13 01:24:02 -0400

    added root script

--- energy_variance/root.C
+++ energy_variance/root.C
@@ -1,34 +1,67 @@
 {
     vector <double> x;
-    vector <double> sim_prob;
-    vector <double> sim_prob_un;
+    vector <double> sim_prob0;
+    vector <double> sim_prob1;
+    vector <double> sim_prob2;
+    vector <double> sim_prob3;
+    vector <double> sim_prob4;
     vector <double> cos_sqr;
     ifstream input("dump.txt");
-    double a,b,c,d;
-    while(input>>a>>b>>c){
-        x.push_back(a);
-        sim_prob.push_back(b);
-        // sim_prob_un.push_back(c);
-        cos_sqr.push_back(c);
+    double temp[11];
+    for(int i = 0; i<20; i++){
+        for(int j = 0; j<11; j++){
+            input>>temp[j];
+        }
+        x.push_back(temp[0]);
+        sim_prob0.push_back(temp[1]);
+        sim_prob1.push_back(temp[3]);
+        sim_prob2.push_back(temp[5]);
+        sim_prob3.push_back(temp[7]);
+        sim_prob4.push_back(temp[9]);
     }
     TMultiGraph *mg = new TMultiGraph();
     // TGraph *gr1 = new TGraphErrors(x.size(), &x[0], &sim_prob[0], NULL, &uncertainty[0]);
-    TGraph *gr1 = new TGraph(x.size(), &x[0], &sim_prob[0]);
-    TGraph *gr2 = new TGraph(x.size(), &x[0], &cos_sqr[0]);
-    // TGraph *gr3 = new TGraph(x.size(), &x[0], &sim_prob_un[0]);
+    TGraph *gr1 = new TGraph(x.size(), &x[0], &sim_prob0[0]);
+    TGraph *gr2 = new TGraph(x.size(), &x[0], &sim_prob1[0]);
+    TGraph *gr3 = new TGraph(x.size(), &x[0], &sim_prob2[0]);
+    TGraph *gr4 = new TGraph(x.size(), &x[0], &sim_prob3[0]);
+    TGraph *gr5 = new TGraph(x.size(), &x[0], &sim_prob4[0]);
+
+    auto gr1_1 = new TGraph(); gr1_1->SetLineColor(1);
+    auto gr2_1 = new TGraph(); gr2_1->SetLineColor(2);
+    auto gr3_1 = new TGraph(); gr3_1->SetLineColor(3);
+    auto gr4_1 = new TGraph(); gr4_1->SetLineColor(4);
+    auto gr5_1 = new TGraph(); gr5_1->SetLineColor(6);
+    
+    for(int i = 0; i<20; i++){
+        gr1_1->SetPoint(i, x[i], sim_prob0[0] * TMath::Cos(3.0));
+        /* gr2_1->SetPoint(i, x[i], simprob1[0] * TMath::Power(TMath::Cos(x[i] / 180.0 * 3.14), 2.0));
+        gr3_1->SetPoint(i, x[i], simprob2[0] * TMath::Power(TMath::Cos(x[i] / 180.0 * 3.14), 2.0));
+        gr4_1->SetPoint(i, x[i], simprob3[0] * TMath::Power(TMath::Cos(x[i] / 180.0 * 3.14), 2.0));
+        gr5_1->SetPoint(i, x[i], simprob4[0] * TMath::Power(TMath::Cos(x[i] / 180.0 * 3.14), 2.0)); */
+    }
     auto c3 = new TCanvas("c3","c3");
     
-    gr1->SetTitle("Simulated Probability");
-    gr2->SetTitle("Cos^2(Theta) fit curve");
-    // gr3->SetTitle("Simulated Probability without energy loss");
+    gr1->SetTitle("Initial_KE = 3 GeV"); gr1->SetMarkerStyle(8); gr1->SetMarkerColor(1); 
+    gr2->SetTitle("Initial_KE = 4 GeV"); gr2->SetMarkerStyle(8); gr2->SetMarkerColor(2);
+    gr3->SetTitle("Initial_KE = 5 GeV"); gr3->SetMarkerStyle(8); gr3->SetMarkerColor(3);
+    gr4->SetTitle("Initial_KE = 6 GeV"); gr4->SetMarkerStyle(8); gr4->SetMarkerColor(4);
+    gr5->SetTitle("Initial_KE = 7 GeV"); gr5->SetMarkerStyle(8); gr5->SetMarkerColor(6);
 
-    gr2->SetLineColor(kBlue);
-    gr1->SetMarkerStyle(8);
-    // gr3->SetMarkerStyle(5);
+    
     mg->Add(gr1, "AP");
-    mg->Add(gr2, "AL");
-    // mg->Add(gr3, "AP");
-    mg->SetTitle("0.1KM seperation; Angle from Z-axis; Survival Percentage"); 
+    mg->Add(gr2, "AP");
+    mg->Add(gr3, "AP");
+    mg->Add(gr4, "AP");
+    mg->Add(gr5, "AP");
+
+    mg->Add(gr1_1, "AL");
+    mg->Add(gr2_1, "AL");
+    mg->Add(gr3_1, "AL");
+    mg->Add(gr4_1, "AL");
+    mg->Add(gr5_1, "AL");
+
+    mg->SetTitle("0.1KM seperation; Angle from Z-axis; Hit Percentage"); 
     mg->Draw("A");
     
     c3->BuildLegend();
